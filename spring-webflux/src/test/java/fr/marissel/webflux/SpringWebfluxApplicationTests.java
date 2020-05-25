c26f297b1c880022d79ad4be314a08b891abbc48
Fabien Marissel <marissel.f@sfeir.com>
2020-05-26 00:26:49 +0200

    initial commit

---
+++ spring-webflux/src/test/java/fr/marissel/webflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.marissel.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
